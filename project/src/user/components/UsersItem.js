680e21c37588ac060479e90dc3ec723d1457b798
BeomSu-Kang <kbs21c1987@gmail.com>
2023-05-23 23:01:20 +0900

    card 스타일링

--- project/src/user/components/UsersItem.js
+++ project/src/user/components/UsersItem.js
@@ -1,19 +1,24 @@
 import "./UsersItem.css";
+import Avatar from "../../shared/components/UiElement/Avatar";
+import { Link } from "react-router-dom";
+import Card from "../../shared/components/UiElement/Card";
 
 const UsersItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
