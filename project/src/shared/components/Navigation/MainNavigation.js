4d5f4901090a0cfd5f83a2262fdaa646d988ccbc
BeomSu-Kang <kbs21c1987@gmail.com>
2023-05-24 21:35:31 +0900

    네비게이션 추가

--- project/src/shared/components/Navigation/MainNavigation.js
+++ project/src/shared/components/Navigation/MainNavigation.js
@@ -3,6 +3,7 @@ import "./MainNavigation.css";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 
 const MainNavigation = (props) => {
   return (
@@ -15,7 +16,9 @@ const MainNavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">Your Place</Link>
       </h1>
-      <nav>navi</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
