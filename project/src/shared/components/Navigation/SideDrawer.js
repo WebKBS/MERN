0f9af04322916bac2af3d557b81b5699e80f0ca1
BeomSu-Kang <kbs21c1987@gmail.com>
2023-05-24 22:05:00 +0900

    react portal, reactDom을 사용해서 포털만들기

--- project/src/shared/components/Navigation/SideDrawer.js
+++ project/src/shared/components/Navigation/SideDrawer.js
@@ -1,6 +1,9 @@
 import "./SideDrawer.css";
+import ReactDom from "react-dom";
 
 const SiderDrawer = (props) => {
-  return <aside className="side-drawer">{props.children}</aside>;
+  const content = <aside className="side-drawer">{props.children}</aside>;
+
+  return ReactDom.createPortal(content, document.getElementById("drawer-hook"));
 };
 export default SiderDrawer;
