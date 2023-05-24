9d9764f71085ca16cf55328037314dd5ef1cb9a5
BeomSu-Kang <kbs21c1987@gmail.com>
2023-05-24 22:16:59 +0900

    사이드메뉴 on off 구현

--- project/src/shared/components/Navigation/MainNavigation.js
+++ project/src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainNavigation.css";
 
 import { Link } from "react-router-dom";
@@ -6,17 +6,31 @@ import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UiElement/Backdrop";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
           <span />
           <span />
