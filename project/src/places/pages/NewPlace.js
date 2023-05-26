504ae683ecd8383b002cc636d6ba92b132ca1b5e
강범수 <kbs21c1987@gmail.com>
2023-05-26 15:40:33 +0900

    input 커스텀 엘레먼트 만들기, input 또는 textarea

--- project/src/places/pages/NewPlace.js
+++ project/src/places/pages/NewPlace.js
@@ -1,5 +1,11 @@
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
 const NewPlace = () => {
-  return <h2>New Place</h2>;
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 };
 
 export default NewPlace;
--- project/src/places/pages/UserPlaces.js
+++ project/src/places/pages/UserPlaces.js
@@ -1,33 +1,31 @@
-import PlaceList from "../components/PlaceList";
+import PlaceList from '../components/PlaceList';
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "헬로우!!",
-    description: "이거슨 디스크립션",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPVipQuhXr5m5_H2x1Teu2HpulWpV2KDG4tg&usqp=CAU",
-    address: "이거슨 주소",
+    id: 'p1',
+    title: '헬로우!!',
+    description: '이거슨 디스크립션',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPVipQuhXr5m5_H2x1Teu2HpulWpV2KDG4tg&usqp=CAU',
+    address: '이거슨 주소',
     location: {
       lat: 0,
       lng: 0,
     },
-    creator: "u1",
+    creator: 'u1',
   },
   {
-    id: "p2",
-    title: "바이!!",
-    description: "두번째 디스크립션 ",
-    imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdGp62Rw-a6f3wgYWvZhp8UJ1Mn8jKlvy9bQ&usqp=CAU",
-    address: "이거슨 주소",
+    id: 'p2',
+    title: '바이!!',
+    description: '두번째 디스크립션 ',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdGp62Rw-a6f3wgYWvZhp8UJ1Mn8jKlvy9bQ&usqp=CAU',
+    address: '이거슨 주소',
     location: {
       lat: 0,
       lng: 0,
     },
-    creator: "u2",
+    creator: 'u2',
   },
 ];
 
