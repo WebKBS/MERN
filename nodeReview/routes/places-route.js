60a6c3f8d86b567e2a0003b83e840ec06117c08e
gangbeomsu <kbs21c1987@gmail.com>
2023-06-23 22:12:05 +0900

    라우트 추가

--- nodeReview/routes/places-route.js
+++ nodeReview/routes/places-route.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/places", (req, res, next) => {
-  console.log("get places");
-  res.json({ message: "WWWWoRRRKK!!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Hello",
+    description: "World",
+    location: {
+      lat: 40.743243,
+      lng: -73.32523,
+    },
+    address: "adress",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => p.id === placeId);
+  res.json({ place });
 });
 
 module.exports = router;
