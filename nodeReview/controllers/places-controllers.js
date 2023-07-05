f8d50b3b773bbbae3af8f91de8fd49fe63e36879
gangbeomsu <kbs21c1987@gmail.com>
2023-07-05 21:57:26 +0900

    patch로 수정하기

--- nodeReview/controllers/places-controllers.js
+++ nodeReview/controllers/places-controllers.js
@@ -68,8 +68,27 @@ const createPlace = (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
+const updatePlaceById = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {};
+
 module.exports = {
   getPlaceById: getPlaceById,
   getPlaceByUserId: getPlaceByUserId,
   createPlace: createPlace,
+  updatePlaceById: updatePlaceById,
+  deletePlace: deletePlace,
 };
