f873d73f0533ca216cd6c3e6220f30eb5be66052
gangbeomsu <kbs21c1987@gmail.com>
2023-07-08 22:49:11 +0900

    데이터베이스 오류 수정

--- mongodb/mongo.js
+++ mongodb/mongo.js
@@ -16,14 +16,14 @@ const createProduct = async (req, res, next) => {
     const db = client.db();
 
     // db 생성
-    const result = db.collection("products").insertOne(newProduct);
+    const result = await db.collection("products").insertOne(newProduct);
+    console.log("새로운 제품이 생성되었습니다:", result.insertedId);
   } catch (error) {
     console.log(error);
-    return res.json({ message: "데이터베이스 에러" });
+    return res.status(500).json({ message: "데이터베이스 에러" });
+  } finally {
+    await client.close();
   }
-
-  // 생성 후에는 반드시 닫아줘야한다.
-  client.close();
 
   res.json(newProduct);
 };
@@ -37,9 +37,10 @@ const getProducts = async (req, res, next) => {
     products = await db.collection("products").find().toArray();
   } catch (error) {
     console.log(error);
-    return res.json({ message: "데이터베이스 가져오기 실패" });
+    return res.status(500).json({ message: "데이터베이스 가져오기 실패" });
+  } finally {
+    await client.close();
   }
-  client.close();
 
   res.json(products);
 };
