81d241378170440262c3b7f09b5dad464a20f2c6
gangbeomsu <kbs21c1987@gmail.com>
2023-07-08 23:12:14 +0900

    mongoose 사용하기

---
+++ mongodb/mongoose.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const Product = require("./models/product");
+
+const url = process.env.MONGO_URL;
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("데이터베이스 연결 완료!");
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("데이터베이스 연결 실패");
+  });
+
+const createProduct = async (req, res, next) => {
+  const createdProduct = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  });
+
+  const result = await createdProduct.save();
+
+  res.json(result);
+};
+
+exports.createProduct = createProduct;
