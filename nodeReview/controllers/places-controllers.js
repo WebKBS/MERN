c7f01e6e56b15b6d3922bdc1ad27cca529e6afeb
gangbeomsu <kbs21c1987@gmail.com>
2023-07-09 20:37:58 +0900

    사용자로부터 장소 삭제만들기

--- nodeReview/controllers/places-controllers.js
+++ nodeReview/controllers/places-controllers.js
@@ -171,15 +171,25 @@ const deletePlace = async (req, res, next) => {
 
   let place;
   try {
-    place = await Place.findById(placeId);
+    place = await Place.findById(placeId).populate("creator");
   } catch (err) {
     console.log(err);
     const error = new HttpError("삭제가 되지 않았습니다.", 500);
     return next(error);
   }
 
+  if (!place) {
+    const error = new HttpError("제공된 place id가 없습니다.", 404);
+    return next(error);
+  }
+
   try {
-    await place.deleteOne();
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.deleteOne({ session: sess });
+    place.creator.places.pull(place);
+    await place.creator.save({ session: sess });
+    await sess.commitTransaction();
   } catch (err) {
     console.log(err);
     const error = new HttpError("삭제가 되지 않았습니다.", 500);
