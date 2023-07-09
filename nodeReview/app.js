85326c4bef499e3ca5363fb3fb3d1c44fbd8b4f6
gangbeomsu <kbs21c1987@gmail.com>
2023-07-09 15:20:34 +0900

    mongoose로 db연결하기

--- nodeReview/app.js
+++ nodeReview/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-route");
 const usersRoutes = require("./routes/users-routes");
@@ -27,4 +28,16 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "요청한 페이지가 없습니다." });
 });
 
-app.listen(4000);
+require("dotenv").config();
+const url = process.env.MONGODB_URL;
+// mongoose가 성공적으로 연결되면 서버 열림
+mongoose
+  .connect(url)
+  .then(() => {
+    app.listen(4000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//mongodb+srv://<username>:<password>@cluster0.gyoifrn.mongodb.net/?retryWrites=true&w=majority
